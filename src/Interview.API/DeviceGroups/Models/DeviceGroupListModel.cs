2e7969731ca555f02acb14aa292b144954f7d892
GencayTuran <gencay.turan@hotmail.com>
2023-12-06 18:55:56 +0100

    refactored model for group totals

--- src/Interview.API/DeviceGroups/Models/DeviceGroupListModel.cs
+++ src/Interview.API/DeviceGroups/Models/DeviceGroupListModel.cs
@@ -2,12 +2,7 @@
 
 public class DeviceGroupListModel
 {
-    private double _power;
-    public string? Group { get; set; }
-    public string? Direction { get; set; }
-    public double Power 
-    {
-        get => _power;
-        set => _power = Math.Round(value, 4);
-    }
+    public string Group { get; set; }
+    public string Direction { get; set; }
+    public TotalPowerModel Power { get; set; }
 }
---
+++ src/Interview.API/DeviceGroups/Models/TotalPowerModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ABB.Interview.API.DeviceGroups.Models
+{
+    public class TotalPowerModel
+    {
+        public double Min { get; set; }
+        public double Max { get; set; }
+        public double Avg { get; set; }
+
+        public TotalPowerModel(double min, double max, double avg)
+        {
+            Min = Math.Round(min, 4);
+            Max = Math.Round(max, 4);
+            Avg = Math.Round(avg, 4);
+        }
+    }
+}
